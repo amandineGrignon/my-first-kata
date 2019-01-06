3aa34a373ecae49082d6ea9bc6e9acbb0ea99d22
Amandine GRIGNON <amandine.grignon@outlook.fr>
2019-01-06 21:46:14 +0100

    Ajout script pour les tests MovieController

---
+++ server/src/test/resources/sql/Test_GetMovies.sql
@@ -0,0 +1,6 @@
+DELETE FROM movies;
+
+INSERT INTO movies (id, title, description)
+VALUES (1, 'Iron Man', 'Tout commence en 2008 avec Iron Man où le Milliardaire Tony Stark, génie un poil mégalomane'),
+       (2, 'Hulk', 'Le Hulk de 2008 fait partie des adaptations au succès limité, mais qui ont pourtant bien leur place dans le MCU.'),
+       (3, 'Iron Man 2', 'En 2010, Iron Man 2 est de retour dans de nouvelles aventures où il doit affronter Whiplash');
