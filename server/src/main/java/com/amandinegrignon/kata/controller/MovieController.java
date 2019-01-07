01744475db1495a158389a8b10f3006bf3cba44d
Amandine GRIGNON <amandine.grignon@outlook.fr>
2019-01-07 19:57:17 +0100

    Limiter le crossOrigin du serveur

--- server/src/main/java/com/amandinegrignon/kata/controller/MovieController.java
+++ server/src/main/java/com/amandinegrignon/kata/controller/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("/api/movies")
 public class MovieController {
 
