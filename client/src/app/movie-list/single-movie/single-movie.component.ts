550fa8b6d4251dcf524812db983e4525807599e2
Amandine GRIGNON <amandine.grignon@outlook.fr>
2019-01-07 19:42:37 +0100

    Page détail d'un film appel au service

--- client/src/app/movie-list/single-movie/single-movie.component.ts
+++ client/src/app/movie-list/single-movie/single-movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movie } from './../../models/movie.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MoviesService } from './../../services/movies.service';
+import { NumberValueAccessor } from '@angular/forms/src/directives';
 
 @Component({
   selector: 'app-single-movie',
@@ -10,7 +11,8 @@ import { MoviesService } from './../../services/movies.service';
 })
 export class SingleMovieComponent implements OnInit {
 
-  movie: Movie;
+  movie: any;
+  displaySpinner = false;
 
   constructor(private route: ActivatedRoute,
               private moviesService: MoviesService,
@@ -20,16 +22,28 @@ export class SingleMovieComponent implements OnInit {
     // Création d'un film vide temporaire, le temps de récupérer celui du serveur
     this.movie = new Movie('', '');
 
+    // Récupération de l'id dans la route
     const id = this.route.snapshot.params['id'];
-
     console.log('id movie: ' + id);
 
-    // +id pour le cast en tant que nombre
-    // this.moviesService.getSingleMovie(+id).then(
-    //  (movie: Movie) => {
-    //    this.movie = movie;
-    //  }
-    // );
+    // Récupération du film depuis le serveur
+    this.getMovie(id);
+  }
+
+  getMovie(id: number) {
+    this.displaySpinner = true;
+
+    try {
+      this.moviesService.getSingleMovie(id).then(
+        data => {
+         this.displaySpinner = false;
+         console.log(data, 'Récupération du film');
+         this.movie = data;
+       });
+     } catch (e) {
+        this.displaySpinner = false;
+        console.log(e, 'Exception lors de la récupération du film');
+     }
   }
 
   onBack() {
